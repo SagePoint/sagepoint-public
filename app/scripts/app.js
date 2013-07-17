545f17a24684da257eeeef06bd2b9898c6969503
Jarad Delorenzo <jarad.delorenzo@warbyparker.com>
2013-07-17 11:43:53 -0400

    Added request survey

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('sagepointApp', [])
+angular.module('sagepointApp', ['ui.bootstrap'])
 .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
     .when('/', {
